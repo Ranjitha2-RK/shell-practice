d8f95f92e2deeb4e0869e35de7d3cff95df8a914
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-21 18:39:50 +0530

    Some Message

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,6 +3,6 @@
 #Invisible text like giving the passwords
 
 echo "Please enter your PIN Number"
-read -s -P PIN_NUMBER # here PIN_NUMBER is variable
+read -s PIN_NUMBER # here PIN_NUMBER is variable
 
 echo "PIN entered is: $PIN_NUMBER"
