6f8d1860843b7d2df29fd8b1389e8168d2dfb3c8
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-21 17:13:19 +0530

    02-conversation.sh

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+# this is comment can't be executed, Just for information purpose
 echo "Hello world"
+echo "I am Ranjitha"
+echo "I am working in Movate"
---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ram::  Hi Suma, How are you?"
+echo "Suma:: Hi Ram, I am fine and what about you?"
+echo "Ram::  What are you learning recently?"
+echo "Suma:: I am Learning Shell scripting."
