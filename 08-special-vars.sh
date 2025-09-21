ec52bb002d38f6600e21bcfcb399e21137326c26
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-22 00:22:14 +0530

    variables

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -11,6 +11,12 @@ echo "Home directory of user: $HOME"
 echo "PID of this script: $$"
 
 #sleep 50 & => it going to back ground
+#ps -ef| grep sleep
+#ec2-user   21943   21913  0 17:45 ?        00:00:03 bash -c while true; do sleep 1;head -v -n 8 /proc/meminfo; head -v -n 2 /proc/stat /proc/version /proc/uptime /proc/loadavg /proc/sys/fs/file-nr /proc/sys/kernel/hostname; tail -v -n 32 /proc/net/dev;echo '==> /proc/df <==';df -l;echo '==> /proc/who <==';who;echo '==> /proc/end <==';echo '##Moba##'; done
+#ec2-user   39431       1  0 18:33 pts/0    00:00:00 sleep 50
+#ec2-user   39723   21943  0 18:34 ?        00:00:00 sleep 1
+#ec2-user   39725   21916  0 18:34 pts/0    00:00:00 grep --color=auto sleep
+
 sleep 50 &
 
 echo "PID of the last comand in background is: $!"
---
+++ 09-data-types.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Everthing in shell is considered as string
+NUMBER1=100
+NUmBER2=200
+
+sum=$(($NUMBER1+$NUMBER2))
+
+echo "SUM is: ${SUM}"
