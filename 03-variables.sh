6b1aade110ed65281a688e9b1d534a19b9f460fe
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-21 17:49:14 +0530

    Some Message

--- 03-variables.sh
+++ 03-variables.sh
@@ -7,4 +7,4 @@ PERSON2=Rahul
 echo "$PERSON1::  Hi $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine and what about you?"
 echo "$PERSON1::  What are you learning recently?"
-echo "$PERSON2:: I am Learning Shell scripting $PERSON2"
+echo "$PERSON2:: I am Learning Shell scripting $PERSON1"
