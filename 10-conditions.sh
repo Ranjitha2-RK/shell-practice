a8e9a9f08fd634d826482e9d9571e826bf446ecc
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-22 11:58:24 +0530

    message

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -5,10 +5,11 @@ NUMBER=$1
 # -lt means less than
 # -gt means greater than
 # -ne means not equal
+# -eq means equal
 
 if [ $NUMBER -lt 10 ]; then
   echo "Given number $NUMBER is less than 10"
-else
+elif [ $NUMBER -eq 10 ]; then
   echo "Given number $NUMBER is equal to 10"
 else
   echo "Given number $NUMBER is greater than 10"
