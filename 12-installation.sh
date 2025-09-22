b31ad9842e4bda3c286cd3b89cdf954f4b6ceb94
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-22 12:30:53 +0530

    message

---
+++ 12-installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Check if the user has root access or not; if root access then proceed; otherwise stop and tell them to run with root access
+# dnf install mysql -y
+#check exit status, if 0 success
+#other wise shwo the user it is failed
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+  echo "ERROR:: Please run this script with root privelege"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+  echo "ERROR:: Installing MySQL is failing"
+else
+  echo "Installing MySQL is success"
