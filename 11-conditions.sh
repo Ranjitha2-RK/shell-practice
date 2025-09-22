608ef2089f301da37a56d277ef050f9cb739bbfc
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-22 12:07:59 +0530

    message

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -8,7 +8,7 @@
 echo "Please enter the number:"
 read NUMBER
 
-if [ (($NUMBER % 2)) -eq 0 ]; then
+if [ $(($NUMBER % 2)) -eq 0 ]; then
   echo "Given number $NUMBER is Even"
 else
   echo "Given number $NUMBER is Odd"
