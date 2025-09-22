497c8ca9f1f2be9435838ab9a4c26d82e8549c79
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-22 12:06:04 +0530

    message

---
+++ 11-conditions.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#If a number is divide by 2 then even otherwise it is odd
+#15/2
+#take the number; divide by 2 and the get the reminder
+#if it is equal to 0 then it is even; otherwise it is odd
+
+echo "Please enter the number:"
+read NUMBER
+
+if [ (($NUMBER % 2)) -eq 0 ]; then
+  echo "Given number $NUMBER is Even"
+else
+  echo "Given number $NUMBER is Odd"
+fi
