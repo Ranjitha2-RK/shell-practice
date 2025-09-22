0199a940f2893c6e0048b77eb0d045213e679d14
Ranjitha Kalimidi <kalimidiranjitha@gmail.com>
2025-09-22 11:50:37 +0530

    message

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -6,7 +6,7 @@ NUMBER=$1
 # -gt means greater than
 # -ne means not equal
 
-if [ $NUMBER -lt 10]; then
+if [ $NUMBER -lt 10 ]; then
   echo "Given number $NUMBER is less than 10"
 else
   echo "Given number $NUMBER is greater than 10"
